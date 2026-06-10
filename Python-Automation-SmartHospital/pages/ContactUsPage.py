b0a6927602d57dd004178015ebc5eb48e1dd51c3
Tamilarasu143 <tamilarasu143@gmail.com>
2026-06-10 20:06:56 +0530

    conftest.py added

---
+++ Python-Automation-SmartHospital/pages/ContactUsPage.py
@@ -0,0 +1,19 @@
+from selenium.webdriver.common.by import By
+
+
+class ContactUsPage:
+
+    # Contact Us Button
+    contact_us_btn = (By.XPATH, "//a[normalize-space()='Contact Us']")
+
+    # Form Fields
+    name = (By.XPATH, "//input[@id='name']")
+    email = (By.XPATH, "//input[@id='email']")
+    subject = (By.XPATH, "//input[@id='subject']")
+    description = (By.XPATH, "//textarea[@id='description']")
+
+    # Submit Button
+    submit = (By.XPATH, "//input[@name='submit']")
+
+    # Success Message
+    success_message = (By.XPATH, "//div[@class='alert alert-success']")
