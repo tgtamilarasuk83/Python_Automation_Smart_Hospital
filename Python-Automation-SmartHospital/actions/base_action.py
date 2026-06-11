c2e6b9f9eedb5bd6f39053880e166b0b66a68e6a
Tamilarasu143 <tamilarasu143@gmail.com>
2026-06-11 13:31:45 +0530

    AddaNewStock Feature Added

--- Python-Automation-SmartHospital/actions/base_action.py
+++ Python-Automation-SmartHospital/actions/base_action.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 class BaseAction:
 
@@ -18,3 +19,39 @@ class BaseAction:
 
     def is_displayed(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator)).is_displayed()
+    
+    def wait_for_clickable(self, locator):
+        return self.wait.until(
+            EC.element_to_be_clickable(locator)
+        )
+
+    def wait_for_visibility(self, locator):
+        return self.wait.until(
+            EC.visibility_of_element_located(locator)
+        )
+    def wait_for_text_to_be_present(self,locator,text):
+        return self.wait.until(EC.text_to_be_present_in_element(locator,text))
+
+    def move_to_element(self, locator):
+        element = self.wait.until(
+            EC.presence_of_element_located(locator)
+        )
+        self.driver.execute_script(
+            "arguments[0].scrollIntoView({block:'center'});",
+            element
+        )
+    def select_by_visible_text(self, locator, text):
+        element = self.driver.find_element(*locator)
+        Select(element).select_by_visible_text(text)
+    
+    
+    
+    def wait_for_visible(self, locator, timeout=20):
+     return WebDriverWait(self.driver, timeout).until(
+        EC.visibility_of_element_located(locator)
+    )
+
+    def wait_for_clickable(self, locator, timeout=20):
+     return WebDriverWait(self.driver, timeout).until(
+        EC.element_to_be_clickable(locator)
+    )
--- Python-Automation-SmartHospital/tests/test_contactus_test.py
+++ Python-Automation-SmartHospital/tests/test_contactus_test.py
@@ -1,11 +1,26 @@
 from actions.ContactUsAction import ContactUsAction
+import pytest
+from selenium import webdriver   
+
+@pytest.fixture()
+def setup():
+
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+
+    driver.get("https://demo.smart-hospital.in/")
+
+    yield driver
+
+    driver.quit()
 
 
 def test_contact_us(setup):
 
     driver = setup
+
     contact = ContactUsAction(driver)
-   
 
     contact.submit_form(
         "Tamil",
@@ -13,6 +28,5 @@ def test_contact_us(setup):
         "Automation Issue",
         "This is a test message from automation script"
     )
-    assert contact.is_success_message_displayed()
 
-  
+    assert contact.is_success_message_displayed()
