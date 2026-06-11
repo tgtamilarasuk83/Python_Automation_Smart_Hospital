8552b2ee758fa30b19c47fe13da2b36445fe658a
tgtamilarasuk83 <tgtamilarasuk83@gmail.com>
2026-06-11 19:48:51 +0530

    Refactor logger setup to use get_logger function

--- Python-Automation-SmartHospital/utilities/logger.py
+++ Python-Automation-SmartHospital/utilities/logger.py
@@ -1,17 +1,23 @@
 import logging
 import sys
 
-def log_generator():
-    logging.basicConfig(
-        filename="testlogreport.log",
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        force=True
-    )
-    logger = logging.getLogger()
-    console_handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
+def get_logger(name=__name__):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s'
+        )
+
+        file_handler = logging.FileHandler("testlogreport.log")
+        file_handler.setFormatter(formatter)
+
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
+
     return logger
