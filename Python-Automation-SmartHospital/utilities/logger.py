132d9496ad87aa0ec1d495495f792232e01f57dc
tgtamilarasuk83 <tgtamilarasuk83@gmail.com>
2026-06-11 20:03:21 +0530

    Update logger.py

--- Python-Automation-SmartHospital/utilities/logger.py
+++ Python-Automation-SmartHospital/utilities/logger.py
@@ -2,23 +2,30 @@ import logging
 import sys
 
 def get_logger(name=__name__):
+    return _base_logger(name)
+
+def log_generator(name=__name__):
+    return _base_logger(name)
+
+def _base_logger(name):
     logger = logging.getLogger(name)
+
+    if logger.hasHandlers():
+        return logger
+
     logger.setLevel(logging.INFO)
 
-    if not logger.handlers:
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
 
-        formatter = logging.Formatter(
-            '%(asctime)s - %(levelname)s - %(message)s'
-        )
+    file_handler = logging.FileHandler("testlogreport.log")
+    file_handler.setFormatter(formatter)
 
-        file_handler = logging.FileHandler("testlogreport.log", mode="a")
-        file_handler.setFormatter(formatter)
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
 
-        console_handler = logging.StreamHandler(sys.stdout)
-        console_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
-        logger.addHandler(file_handler)
-        logger.addHandler(console_handler)
-
-    logger.propagate = False
     return logger
