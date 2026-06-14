e17d01d21b12f52c039d07afefa7c3c2fce484ad
Harini R G <harinirg26@gmail.com>
2026-06-14 21:32:50 +0530

    added logger in blood stock management

--- Python-Automation-SmartHospital/actions/BloodStockAction.py
+++ Python-Automation-SmartHospital/actions/BloodStockAction.py
@@ -1,32 +1,27 @@
 from actions.base_action import BaseAction
 from pages.BloodStockPage import BloodStockPage
 from selenium.webdriver.common.keys import Keys
+from utilities.logger import log_generator
 
 
 class BloodStockAction(BaseAction):
+
+    log = log_generator()
 
     def __init__(self, driver):
         super().__init__(driver)
 
     def is_blood_stock_status_page_displayed(self):
-        return self.is_displayed(
-            BloodStockPage.blood_bank_status
-        )
+        return self.is_displayed(BloodStockPage.blood_bankstatus)
 
     def select_blood_group(self, blood_group):
-        self.click(
-            BloodStockPage.blood_group_option(blood_group)
-        )
+        self.click(BloodStockPage.blood_group_option(blood_group))
 
     def is_blood_bag_details_displayed(self):
-        return self.is_displayed(
-            BloodStockPage.blood_bag_table
-        )
+        return self.is_displayed(BloodStockPage.blood_bagtable)
 
     def is_blood_component_details_displayed(self):
-        return self.is_displayed(
-            BloodStockPage.component_table
-        )
+        return self.is_displayed(BloodStockPage.component_table)
 
     def clickAddIcon(self):
         self.click(BloodStockPage.add_icon)
@@ -35,40 +30,26 @@ class BloodStockAction(BaseAction):
         return self.is_displayed(BloodStockPage.popup)
 
     def selectBloodDonor(self, donor):
-
         self.click(BloodStockPage.blood_donor)
-
         self.send_keys(BloodStockPage.search_box, donor)
-
         self.click(BloodStockPage.dynamic_option(donor))
 
     def enterDonateDate(self, donate_date):
-
         self.clear(BloodStockPage.donate_date)
-
         self.send_keys(BloodStockPage.donate_date, donate_date)
-
-        self.driver.find_element(
-            *BloodStockPage.donate_date
-        ).send_keys(Keys.TAB)
+        self.driver.find_element(*BloodStockPage.donate_date).send_keys(Keys.TAB)
 
     def enterBag(self, bag):
         self.send_keys(BloodStockPage.bag_field, bag)
 
     def selectChargeCategory(self, category):
-
         self.click(BloodStockPage.charge_category)
-
         self.send_keys(BloodStockPage.search_box, category)
-
         self.click(BloodStockPage.dynamic_option(category))
 
     def selectChargeName(self, charge_name):
-
         self.click(BloodStockPage.charge_name)
-
         self.send_keys(BloodStockPage.search_box, charge_name)
-
         self.click(BloodStockPage.dynamic_option(charge_name))
 
     def clickCalculateButton(self):
@@ -78,19 +59,21 @@ class BloodStockAction(BaseAction):
         self.click(BloodStockPage.save_button)
 
     def isBagNumberDisplayed(self, bag):
-        return self.is_displayed(
-            BloodStockPage.added_bag_number(bag)
-        )
+        status = self.is_displayed(BloodStockPage.added_bag_number(bag))
+        if status:
+            self.log.info("Blood stock record added successfully")
+        return status
 
     def clickIssueButton(self, bag):
         self.click(BloodStockPage.issue_button(bag))
 
     def isBloodIssuePageDisplayed(self):
-        return self.is_displayed(
-            BloodStockPage.issue_date
-        )
+        status = self.is_displayed(BloodStockPage.issue_date)
+        if status:
+            self.log.info("Blood issue page displayed successfully")
+        return status
 
     def getValidationMessage(self):
-        return self.get_text(
-            BloodStockPage.validationMessage
-        )
+        message = self.get_text(BloodStockPage.validationMessage)
+        self.log.info(f"Validation message displayed: {message}")
+        return message
