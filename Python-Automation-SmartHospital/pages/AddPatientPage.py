369f4e46270f09f9185940b84bc60ed243e3cfa6
Jananisri14 <jananisathishkumar380@gmail.com>
2026-06-14 22:56:16 +0530

    Added Two Tests in Add Patient feature

--- Python-Automation-SmartHospital/pages/AddPatientPage.py
+++ Python-Automation-SmartHospital/pages/AddPatientPage.py
@@ -18,4 +18,6 @@ class AddPatientPage:
     address         = (By.XPATH, "//div[@class='col-lg-12 col-md-12 col-sm-12']//input[@name='address']")
     save_button     = (By.XPATH, "//button[@id='formaddpabtn']")
     toast_message   = (By.XPATH, "//div[@class='toast-message']")
+    phone_error      = (By.XPATH, "//p[normalize-space()='Phone field must contain only numbers']")
+    email_error      = (By.XPATH, "//p[normalize-space()='Email field must contain a valid email address']")
     first_row_name  = (By.XPATH, "//table//tbody/tr[1]/td[1]")
