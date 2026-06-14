08844d3926e6be0f8885d6d50d954d4afb0f7976
Harini R G <harinirg26@gmail.com>
2026-06-14 10:46:33 +0530

    Implemented Blood Stock donor creation test

---
+++ Python-Automation-SmartHospital/actions/BloodStockAction.py
@@ -0,0 +1,96 @@
+from actions.base_action import BaseAction
+from pages.BloodStockPage import BloodStockPage
+from selenium.webdriver.common.keys import Keys
+
+
+class BloodStockAction(BaseAction):
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def is_blood_stock_status_page_displayed(self):
+        return self.is_displayed(
+            BloodStockPage.blood_bank_status
+        )
+
+    def select_blood_group(self, blood_group):
+        self.click(
+            BloodStockPage.blood_group_option(blood_group)
+        )
+
+    def is_blood_bag_details_displayed(self):
+        return self.is_displayed(
+            BloodStockPage.blood_bag_table
+        )
+
+    def is_blood_component_details_displayed(self):
+        return self.is_displayed(
+            BloodStockPage.component_table
+        )
+
+    def clickAddIcon(self):
+        self.click(BloodStockPage.add_icon)
+
+    def isBloodDonorPopupDisplayed(self):
+        return self.is_displayed(BloodStockPage.popup)
+
+    def selectBloodDonor(self, donor):
+
+        self.click(BloodStockPage.blood_donor)
+
+        self.send_keys(BloodStockPage.search_box, donor)
+
+        self.click(BloodStockPage.dynamic_option(donor))
+
+    def enterDonateDate(self, donate_date):
+
+        self.clear(BloodStockPage.donate_date)
+
+        self.send_keys(BloodStockPage.donate_date, donate_date)
+
+        self.driver.find_element(
+            *BloodStockPage.donate_date
+        ).send_keys(Keys.TAB)
+
+    def enterBag(self, bag):
+        self.send_keys(BloodStockPage.bag_field, bag)
+
+    def selectChargeCategory(self, category):
+
+        self.click(BloodStockPage.charge_category)
+
+        self.send_keys(BloodStockPage.search_box, category)
+
+        self.click(BloodStockPage.dynamic_option(category))
+
+    def selectChargeName(self, charge_name):
+
+        self.click(BloodStockPage.charge_name)
+
+        self.send_keys(BloodStockPage.search_box, charge_name)
+
+        self.click(BloodStockPage.dynamic_option(charge_name))
+
+    def clickCalculateButton(self):
+        self.click(BloodStockPage.calculate_button)
+
+    def clickSaveButton(self):
+        self.click(BloodStockPage.save_button)
+
+    def isBagNumberDisplayed(self, bag):
+        return self.is_displayed(
+            BloodStockPage.added_bag_number(bag)
+        )
+
+    def clickIssueButton(self, bag):
+        self.click(BloodStockPage.issue_button(bag))
+
+    def isBloodIssuePageDisplayed(self):
+        return self.is_displayed(
+            BloodStockPage.issue_date
+        )
+
+    def getValidationMessage(self):
+        return self.get_text(
+            BloodStockPage.validationMessage
+        )
---
+++ Python-Automation-SmartHospital/tests/test_bloodstock.py
@@ -0,0 +1,78 @@
+import pytest
+from actions.LoginAction import LoginAction
+from actions.BloodIssueAction import BloodIssueAction
+from actions.BloodStockAction import BloodStockAction
+
+from utilities.excel_reader import get_data
+from utilities.csv_reader import get_data as get_csv_data
+
+
+class TestBloodStock:
+
+    @pytest.mark.parametrize(
+        "blood_group",
+        get_data("BloodStock.xlsx", "BloodGroup")
+    )
+    def test_verify_blood_stock_details(
+        self,
+        setup,
+        blood_group
+    ):
+        login = LoginAction(setup)
+        blood_issue = BloodIssueAction(setup)
+        blood_stock = BloodStockAction(setup)
+
+        login.validLogin()
+
+        blood_issue.clickBloodBankMenu()
+
+        assert blood_stock.is_blood_stock_status_page_displayed()
+
+        blood_stock.select_blood_group(blood_group[0])
+
+        assert blood_stock.is_blood_bag_details_displayed()
+
+        assert blood_stock.is_blood_component_details_displayed()
+    
+    @pytest.mark.parametrize(
+        "blood_donor,donate_date,bag,charge_category,charge_name",
+        get_data("BloodStock.xlsx", "AddDonor")
+    )
+    def test_add_blood_donor(
+        self,
+        setup,
+        blood_donor,
+        donate_date,
+        bag,
+        charge_category,
+        charge_name
+    ):
+        login = LoginAction(setup)
+        blood_issue = BloodIssueAction(setup)
+        blood_stock = BloodStockAction(setup)
+
+        login.validLogin()
+
+        blood_issue.clickBloodBankMenu()
+
+        blood_stock.clickAddIcon()
+
+        assert blood_stock.isBloodDonorPopupDisplayed()
+
+        blood_stock.selectBloodDonor(blood_donor)
+
+        blood_stock.enterDonateDate(donate_date)
+
+        blood_stock.enterBag(str(bag))
+
+        blood_stock.selectChargeCategory(charge_category)
+
+        blood_stock.selectChargeName(charge_name)
+
+        blood_stock.clickCalculateButton()
+
+        blood_stock.clickSaveButton()
+
+        assert blood_stock.isBagNumberDisplayed(str(bag))
+
+    
