e447a3be05d626d5986a066c40bd00f85f36a70d
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-06-14 22:44:40 +0530

    new changes made in add ambulance call

--- Python-Automation-SmartHospital/actions/AddAmbulanceCallAction.py
+++ Python-Automation-SmartHospital/actions/AddAmbulanceCallAction.py
@@ -12,27 +12,31 @@ class AddAmmbulanceCallAction(BaseAction):
 
     def __init__(self,driver):
         super().__init__(driver)
+
     
     def validaddambulanceccall(self,patientname,vehicleno,date,chargecate,chargename,standardcharge,note,payment):
-        self.click(HomePage.ambulanceBtn)
-        self.click(AmbulancePage.addambulance)
-        self.click(AddAmbulanceCall.selectPatient)
-        self.send_keys(AddAmbulanceCall.selectPatientBox,patientname)
-        self.click(AddAmbulanceCall.selectPatientList)
-        self.select_by_visible_text(AddAmbulanceCall.vehicleno,vehicleno)
-        self.select_date(AddAmbulanceCall.date,date)
-        self.click(AddAmbulanceCall.chargecate)
-        self.send_keys(AddAmbulanceCall.chargecatebox,chargecate)
-        self.click(AddAmbulanceCall.chargecatelist)
-        self.click(AddAmbulanceCall.chargename)
-        self.wait_for_visibility(AddAmbulanceCall.chargenameul)
-        self.send_keys(AddAmbulanceCall.chargenamebox,chargename)
-        self.click(AddAmbulanceCall.chargenameList)
-        self.wait_for_value_to_be_present(AddAmbulanceCall.standardcharge,standardcharge)
-        self.send_keys(AddAmbulanceCall.note,note)
-        self.wait_for_value_to_be_present(AddAmbulanceCall.payment,payment)
-        self.click(AddAmbulanceCall.savebtn)
-        self.log.info("Added New add ambulance call record")
+        try:
+            self.click(HomePage.ambulanceBtn)
+            self.click(AmbulancePage.addambulance)
+            self.click(AddAmbulanceCall.selectPatient)
+            self.send_keys(AddAmbulanceCall.selectPatientBox,patientname)
+            self.click(AddAmbulanceCall.selectPatientList)
+            self.select_by_visible_text(AddAmbulanceCall.vehicleno,vehicleno)
+            self.select_date(AddAmbulanceCall.date,date)
+            self.click(AddAmbulanceCall.chargecate)
+            self.send_keys(AddAmbulanceCall.chargecatebox,chargecate)
+            self.click(AddAmbulanceCall.chargecatelist)
+            self.click(AddAmbulanceCall.chargename)
+            self.wait_for_visibility(AddAmbulanceCall.chargenameul)
+            self.send_keys(AddAmbulanceCall.chargenamebox,chargename)
+            self.click(AddAmbulanceCall.chargenameList)
+            self.wait_for_value_to_be_present(AddAmbulanceCall.standardcharge,standardcharge)
+            self.send_keys(AddAmbulanceCall.note,note)
+            self.wait_for_value_to_be_present(AddAmbulanceCall.payment,payment)
+            self.click(AddAmbulanceCall.savebtn)
+            self.log.info("Added New add ambulance call record")
+        except Exception as e:
+            self.driver.save_screenshot("Screenshots/addambulancecall.png")
     
 
     def invalidaddambulanceccall(self):
@@ -46,7 +50,7 @@ class AddAmmbulanceCallAction(BaseAction):
         self.click(AddAmbulanceCall.chargecate)
         self.send_keys(AddAmbulanceCall.chargecatebox,get_value("config.ini","ambulance details","chargecate"))
         self.click(AddAmbulanceCall.chargecatelist)
-        self.send_keys(AddAmbulanceCall.note,("config.ini","ambulance details","note"))
+        self.send_keys(AddAmbulanceCall.note,get_value("config.ini","ambulance details","note"))
         self.click(AddAmbulanceCall.savebtn)
         self.log.info("added ambulance call with not filling required field")
     
