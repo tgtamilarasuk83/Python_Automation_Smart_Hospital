2bf6df03745a2129a915985f4aae8b952cac690b
tgtamilarasuk83 <tgtamilarasuk83@gmail.com>
2026-06-15 14:03:05 +0530

    Update base_action.py

--- Python-Automation-SmartHospital/actions/base_action.py
+++ Python-Automation-SmartHospital/actions/base_action.py
@@ -11,36 +11,56 @@ class BaseAction:
         self.wait = WebDriverWait(self.driver, 10)
 
     def click(self, locator):
-        self.wait.until(EC.visibility_of_element_located(locator)).click()
+        self.wait.until(
+            EC.visibility_of_element_located(locator)
+        ).click()
 
     def send_keys(self, locator, value):
-        self.wait.until(EC.visibility_of_element_located(locator)).send_keys(value)
+        self.wait.until(
+            EC.visibility_of_element_located(locator)
+        ).send_keys(value)
 
     def get_text(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator)).text
+        return self.wait.until(
+            EC.visibility_of_element_located(locator)
+        ).text
 
     def is_displayed(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator)).is_displayed()
+        return self.wait.until(
+            EC.visibility_of_element_located(locator)
+        ).is_displayed()
 
     def wait_for_clickable(self, locator):
-        return self.wait.until(EC.element_to_be_clickable(locator))
+        return self.wait.until(
+            EC.element_to_be_clickable(locator)
+        )
 
     def wait_for_visibility(self, locator):
-        return self.wait.until(EC.visibility_of_element_located(locator))
+        return self.wait.until(
+            EC.visibility_of_element_located(locator)
+        )
 
     def wait_for_text_to_be_present(self, locator, text):
-        return self.wait.until(EC.text_to_be_present_in_element(locator, text))
+        return self.wait.until(
+            EC.text_to_be_present_in_element(locator, text)
+        )
 
     def wait_for_value_to_be_present(self, locator, text):
-        return self.wait.until(EC.text_to_be_present_in_element_value(locator, text))
+        return self.wait.until(
+            EC.text_to_be_present_in_element_value(locator, text)
+        )
 
     def select_by_visible_text(self, locator, text):
-        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element = self.wait.until(
+            EC.visibility_of_element_located(locator)
+        )
         select = Select(element)
-        return select.select_by_visible_text(text)
+        select.select_by_visible_text(text)
 
     def hover(self, locator):
-        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element = self.wait.until(
+            EC.visibility_of_element_located(locator)
+        )
         ActionChains(self.driver).move_to_element(element).perform()
 
     def handle_alert(self):
@@ -49,24 +69,38 @@ class BaseAction:
             alert = self.driver.switch_to.alert
             alert.accept()
             return True
-        except:
+        except Exception:
             return False
 
     def clear(self, locator):
-        self.wait.until(EC.visibility_of_element_located(locator)).clear()
+        self.wait.until(
+            EC.visibility_of_element_located(locator)
+        ).clear()
 
-    def select_date(self, element, date):
-        ele = self.driver.find_element(*element)
-        self.driver.execute_script("arguments[0].value=arguments[1];", ele, date)
+    def select_date(self, locator, date):
+        element = self.driver.find_element(*locator)
+        self.driver.execute_script(
+            "arguments[0].value=arguments[1];",
+            element,
+            date
+        )
 
     def jsclick(self, locator):
-        ele = self.wait.until(EC.visibility_of_element_located(locator))
-        self.driver.execute_script("arguments[0].click();", ele)
+        element = self.wait.until(
+            EC.visibility_of_element_located(locator)
+        )
+        self.driver.execute_script(
+            "arguments[0].click();",
+            element
+        )
 
     def move_to_element(self, locator):
-        element = self.wait.until(EC.presence_of_element_located(locator))
+        element = self.wait.until(
+            EC.presence_of_element_located(locator)
+        )
         self.driver.execute_script(
-            "arguments[0].scrollIntoView({block:'center'});", element
+            "arguments[0].scrollIntoView({block:'center'});",
+            element
         )
 
     def wait_for_visible(self, locator, timeout=20):
