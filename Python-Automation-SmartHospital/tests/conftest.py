242a94c1b61e8efdf9421b47d87d74585e10c7fc
Balamurugan <murugan020325@gmail.com>
2026-06-15 20:01:04 +0530

    Update headless option for Chrome WebDriver

--- Python-Automation-SmartHospital/tests/conftest.py
+++ Python-Automation-SmartHospital/tests/conftest.py
@@ -5,7 +5,7 @@ from utilities.config_reader import get_value
 @pytest.fixture()
 def setup():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
